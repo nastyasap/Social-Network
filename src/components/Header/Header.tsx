e1fdead604d3c31d8212a3ee25e6661938b8a57e
Анастасия Сапожникова <nastyasap53@gmail.com>
2022-02-04 14:26:17 +0300

    lesson17

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Header.module.css'
 
 export const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src='https://venngage-wordpress.s3.amazonaws.com/uploads/2019/04/Travel-Tour-Business-Logo.png'
                  alt='Logo'/>
         </header>)
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,23 +1,18 @@
 import React from "react";
+import s from './Profile.module.css'
+import {MyPosts} from "./Posts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div>
+        <div className={s.content}>
             <div>
                 <img src='https://www.kartinki24.ru/uploads/gallery/main/374/kartinki24_ru_autumn_273.jpg'/>
             </div>
             <div>
                 Ava+Description
-                <img src='https://russkiiyazyk.ru/wp-content/uploads/2018/06/Kartinka.jpg'/>
+                <img/>
             </div>
-            <div>
-                My posts
-                <div>New Post</div>
-            </div>
-            <div>
-                <div>Post 1</div>
-                <div>Post 2</div>
-            </div>
+            <MyPosts/>
         </div>
     )
 }
